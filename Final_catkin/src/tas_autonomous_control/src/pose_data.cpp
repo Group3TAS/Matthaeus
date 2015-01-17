0d1820fc5a7c1757fb088f7c55e5a0737100a344
Group3TAS <philipp.schmitt.ps@googlemail.com>
2015-01-17 18:39:10 +0100

    Update pose_data.cpp

--- Final_catkin/src/tas_autonomous_control/src/pose_data.cpp
+++ Final_catkin/src/tas_autonomous_control/src/pose_data.cpp
@@ -5,51 +5,57 @@
 #include <geometry_msgs/PoseStamped.h>
 #include "wlan_data.h"
 
+/*
+
+Technik Autonomer Systeme
+
+17 January 2015
+
+Group 3
+
+WLAN and Pose data reading and recording
+
+*/
+
 using std::vector;
 using std::cout;
 using std::endl;
 
-//void chatterCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
-//{
-//   ROS_INFO("I heard: [%f]", msg->pose.pose.position.x);
-//}
-
 void nextCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
-  ROS_INFO("X Coordinate: %f", msg->pose.pose.position.x);
-  ROS_INFO("Y Coordinate: %f", msg->pose.pose.position.y);
-  ROS_INFO("Z Coordinate: %f", msg->pose.pose.position.z);
 
+  // Retrieves size of /amcl_pose's covariance matrix
+  // and saves it into a vector
   int x = msg->pose.covariance.size();
   vector<float> cov_vec, orient_vec;
+
   for (int i = 0; i != x ; i++)
   {
       cov_vec.push_back(msg->pose.covariance[i]);
       cout << cov_vec[i] << endl;
   }
 
+  // Saves the quarternion data into a vector
   orient_vec.push_back(msg->pose.pose.orientation.z);
   orient_vec.push_back(msg->pose.pose.orientation.w);
+
+  // Calls the following function to retrieve a strength reading
+  // on all available WLAN signals and combines the readings to
+  // the pose data into a simicolon separated line which is saved
+  // into a text file
   wlan_data_write(msg->pose.pose.position.x,msg->pose.pose.position.y,cov_vec,orient_vec);
+
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+  // Create Node
   ros::init(argc, argv, "pose_data_reader");
 
   ros::NodeHandle n;
 
+  // Create a subscriber callback function each time pose data
+  // is received
   ros::Subscriber sub = n.subscribe("amcl_pose", 1000, nextCallback);
-  //ros::Subscriber sub2 = n.subscribe("move_base_simple/goal",1000,chatterCallback);
 
   ros::spin();
 
